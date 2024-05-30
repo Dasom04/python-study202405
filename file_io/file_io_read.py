4300034b33af839824c217ecb7fe12ed604d0f21
Dasom04 <slz_952@naver.com>
2024-05-30 11:47:55 +0900

    #8. 파일 입/출력

---
+++ file_io/file_io_read.py
@@ -0,0 +1,56 @@
+
+'''
+* 파일 읽기 기능 (read)
+
+- 파일로부터 데이터를 읽어들일 때는 분량에 따라
+적당한 메서드를 선택해서 사용합니다.
+
+1. read(): 파일 전체를 통째로 읽어서 리턴.
+2. readline(): 파일 데이터를 한 줄씩 읽어서 리턴.
+3. readlines(): 파일 전체를 읽어서 한 줄씩 분리한 후에
+리스트에 담아서 리턴.
+'''
+
+file_path = r'C:\MyWorkSpace\upload\test.txt'
+'''
+try:
+    f = open(file_path, 'r')
+    text = f.read()
+    print(text)
+except:
+    print('파일 로드 실패!')
+finally:
+    f.close()
+'''
+
+# readline() 메서드는 자동으로 \n을 기준으로 하여
+# 데이터를 줄 단위로 읽어들이기 때문에 메모리 부담을 좀 더
+# 줄일 수 있습니다.
+'''
+try:
+    f = open(file_path, 'r')
+    text = f.readline() # 내용의 첫번째 줄만 나오게 한다.
+    print(text)
+    text = f.readline() # 밑에 하나 더 쓰면 두번째 줄이 나온다.
+    print(text)
+except:
+    print('파일 로드 실패!')
+finally:
+    f.close()
+'''
+
+# readlines()는 파일 데이터를 한 줄씩 읽어서
+# 리스트에 담아서 리턴하기 떄문에 읽은 데이터를
+# 리스트 문법을 사용하여 처리할 수 있습니다.
+try:
+    f = open(file_path, 'r')
+    text = f.readlines() # 리스트로 나온다.
+    print(type(text))
+    text.reverse()
+    for t in text:
+        print(t)
+except:
+    print('파일 로드 실패!')
+finally:
+    f.close()
+
