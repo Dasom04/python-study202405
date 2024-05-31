f0d9ae62a8de0d33f30b098d1e81ba243aeded51
Dasom04 <slz_952@naver.com>
2024-05-31 15:29:42 +0900

    #10. 크롤링 데이터 다양하게 출력하기(txt, xlsx)

--- web_crawliong/crawler_aladin03.py
+++ web_crawliong/crawler_aladin03.py
@@ -32,6 +32,78 @@ driver.find_element(By.XPATH, '//*[@id="Wa_header1_headerTop"]/div[2]/div[3]/ul[
 
 t.sleep(2)
 
+
+with codecs.open(file_path, mode='w', encoding='utf-8') as f:
+
+    cur_page_num = 2 # 현재 페이지 넘버
+    target_page_num = 9 # 목적지 페이지 번호
+    rank = 1 # 순위
+
+
+    while cur_page_num <= target_page_num:
+
+        for i in range(3, 11):
+            src = driver.page_source
+
+            soup = BeautifulSoup(src, 'html.parser')
+
+            div_list = soup.find_all('div', class_='ss_book_box')
+            # div_list = soup.select('div.ss_book_box') -> 가능
+
+            for div in div_list:
+                book_info = div.find_all('li')
+
+                if book_info[0].find('span', class_='ss_ht1') == None:
+                    book_title = book_info[0].text
+                    book_author = book_info[1].text
+                    book_price = book_info[2].text
+                else:
+                    book_title = book_info[1].text
+                    book_author = book_info[2].text
+                    book_price = book_info[3].text
+
+                auth_info = book_author.split(' | ')
+
+                f.write(f'순위: {rank}위\n')
+                f.write(f'제목: {book_title}\n')
+                f.write(f'저자: {auth_info[0]}\n')
+                f.write(f'출판사: {auth_info[1]}\n')
+                f.write(f'출판월일: {auth_info[2]}\n') 
+                f.write(f'가격: {book_price.split(", ")[0]}\n')
+                f.write('-' * 40+ '\n')
+
+                rank += 1
+        
+            # 다음 페이지(웹)로 전환 
+            cur_page_num += 1
+            driver.find_element(By.XPATH, f'//*[@id="newbg_body"]/div[3]/ul/li[{cur_page_num}]/a').click()
+            
+            del soup
+
+            t.sleep(3) # 렌더링 되는 시간 벌어주기 (여유있게 주기)
+
+
+'''
+find(태그이름,클래스_='' or id=''): 조건에 맞는 첫 번째 요소를 반환합니다.
+find_all(태그이름,클래스_='' or id=''): 조건에 맞는 모든 요소를 리스트 형태로 반환합니다.
+select(선택자): CSS 선택자를 사용해 요소를 선택합니다.
+select_one(선택자): CSS 선택자를 사용해 첫 번째 요소를 선택합니다.
+find_parent(): 해당 요소의 부모 요소를 반환합니다.
+find_parents(): 조건에 맞는 모든 부모 요소를 리스트 형태로 반환합니다.
+find_next_sibling(): 다음 형제 요소를 반환합니다.
+find_next_siblings(): 조건에 맞는 모든 다음 형제 요소를 리스트 형태로 반환합니다.
+find_previous_sibling(): 이전 형제 요소를 반환합니다.
+find_previous_siblings(): 조건에 맞는 모든 이전 형제 요소를 리스트 형태로 반환합니다.
+find_next(): 다음 요소를 반환합니다.
+find_all_next(): 조건에 맞는 모든 다음 요소를 리스트 형태로 반환합니다.
+find_previous(): 이전 요소를 반환합니다.
+find_all_previous(): 조건에 맞는 모든 이전 요소를 리스트 형태로 반환합니다.
+'''
+
+
+
+'''
+* 내가 작성한것
 with codecs.open(file_path, mode='w', encoding='utf-8') as al:
    
     rank = 1
@@ -72,6 +144,6 @@ with codecs.open(file_path, mode='w', encoding='utf-8') as al:
         if i == 11:
             break
         driver.find_element(By.XPATH, f'//*[@id="newbg_body"]/div[3]/ul/li[{i}] /a').click()
-            
+'''            
             
             
